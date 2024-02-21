e53c99fecf8bed21e872c18247ad712ce90f7350
小冷睡着了 <2455880592@qq.com>
2024-02-21 09:47:44 +0800

    新增vercel埋点监控     在main.ts中添加

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <!-- App.vue -->
 <script setup lang="ts">
-import { inject } from "@vercel/analytics";
 import zhCN from "ant-design-vue/es/locale/zh_CN";
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
 import { ref } from "vue";
 dayjs.locale("zh-cn");
 const locale = ref(zhCN);
-
-inject();
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,9 @@ import "./common/css/style.css";
 import echarts from "@/common/ts/echarts";
 import antdConfig from "@/config/antd.config";
 import i18n from "@/locales";
+import { inject } from "@vercel/analytics";
 
+inject()
 antdConfig();
 
 const pinia = createPinia();
