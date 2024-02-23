74b0bc371e55fbe0348cae6917f2daa264958423
小冷睡着了 <2455880592@qq.com>
2024-02-23 15:40:00 +0800

    部分优化

--- src/views/Routine/Info/components/Edit/index.vue
+++ src/views/Routine/Info/components/Edit/index.vue
@@ -100,28 +100,28 @@ const handleAvatarUploadChange = ({ file }: any) => {
       >
         <a-form-item label="用户名">
           <a-input
-            v-model="formInfo.username"
+            v-model:value="formInfo.username"
             placeholder="请输入用户名"
             disabled
           />
         </a-form-item>
         <a-form-item label="用户昵称" v-bind="validateInfos.nickname">
-          <a-input v-model="formInfo.nickname" placeholder="请输入用户昵称" />
+          <a-input v-model:value="formInfo.nickname" placeholder="请输入用户昵称" />
         </a-form-item>
         <a-form-item label="邮箱地址">
-          <a-input v-model="formInfo.email" placeholder="请输入邮箱地址" />
+          <a-input v-model:value="formInfo.email" placeholder="请输入邮箱地址" />
         </a-form-item>
         <a-form-item label="手机号码">
-          <a-input v-model="formInfo.email" placeholder="请输入手机号码" />
+          <a-input v-model:value="formInfo.email" placeholder="请输入手机号码" />
         </a-form-item>
         <a-form-item label="签名">
           <a-textarea
-            v-model="formInfo.motto"
+            v-model:value="formInfo.motto"
             placeholder="这家伙很懒，什么都没有留下"
           />
         </a-form-item>
         <a-form-item label="新密码">
-          <a-input v-model="formInfo.email" placeholder="不修改请留空" />
+          <a-input v-model:value="formInfo.email" placeholder="不修改请留空" />
         </a-form-item>
       </a-form>
     </div>
--- src/views/Routine/Info/index.vue
+++ src/views/Routine/Info/index.vue
@@ -6,12 +6,16 @@ import Log from "./components/Log/index.vue";
 
 <template>
   <a-row class="default-main" :gutter="[16, 16]">
-    <a-col :lg="12" :xs="24">
-      <Edit class="is-always-shadow" />
+    <a-col :lg="12" :xs="24" class="default-main-col">
+      <edit class="is-always-shadow" />
     </a-col>
-    <a-col :lg="12" :xs="24">
-      <a-card title="操作日志" :bordered="false" class="is-always-shadow operate-log">
-        <Log />
+    <a-col :lg="12" :xs="24" class="default-main-col">
+      <a-card
+        title="操作日志"
+        :bordered="false"
+        class="is-always-shadow operate-log"
+      >
+        <log />
       </a-card>
     </a-col>
   </a-row>
