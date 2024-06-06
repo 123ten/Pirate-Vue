9bca071f8cefafc7d01b12465cbb1116caec3d72
小冷睡着了. <2455880592@qq.com>
2024-06-06 09:45:47 +0800

    feat: 新增 navBar 导航栏滚动过渡效果

--- src/components/Layout/components/Header/components/TagsPc/index.vue
+++ src/components/Layout/components/Header/components/TagsPc/index.vue
@@ -3,7 +3,7 @@
 import * as antIcons from "@ant-design/icons-vue";
 import {CloseOutlined, DownOutlined} from "@ant-design/icons-vue";
 import {defineOptions, nextTick, onMounted, reactive, ref,} from "vue";
-import {onBeforeRouteUpdate, RouteLocationNormalized, useRoute, useRouter} from "vue-router";
+import {onBeforeRouteUpdate, RouteLocationNormalized, useRouter} from "vue-router";
 import {storeToRefs} from "pinia";
 import {useLayoutStore} from "@/store";
 import useNavTabs from "@/store/hooks/useNavTabs";
@@ -16,16 +16,10 @@ const {isLayoutFullScreen, isCurrentPageReload, isAsideMenu} = storeToRefs(layou
 const navTabs = useNavTabs()
 
 const router = useRouter();
-const route = useRoute();
-
-interface ITagItem {
-  title: string;
-  path: string;
-}
 
 interface MouseRightStateInterface {
-  index: number;
-  data: ITagItem;
+  activeIndex: number;
+  route: RouteLocationNormalized;
 }
 
 enum OverlayEnum {
@@ -39,19 +33,10 @@ enum OverlayEnum {
 const tabsRefs = ref();
 const tabItemRefs = ref<HTMLDivElement[]>([])
 
-const tagState = reactive({
-  width: 2 * 14 + 32,
-  x: 0,
+const contextMenuState = reactive<MouseRightStateInterface>({
+  activeIndex: 0,
+  route: router.currentRoute.value
 });
-const mouseRightState = reactive<MouseRightStateInterface>({
-  index: 0,
-  data: {
-    title: "",
-    path: "",
-  },
-});
-
-const hoverIndex = ref<number>(-1);
 
 onMounted(async () => {
   // 这里的数据不需要被响应式包裹
@@ -70,20 +55,6 @@ const updateTab = async (to: RouteLocationNormalized) => {
   selectNavTab(tabItemRefs.value[navTabs.activeIndex])
 }
 
-
-/**
- * 修改x轴偏移量
- * @param index
- */
-const changeOffsetLeft = async (index: number, type?: 'enter' | 'level') => {
-  if (type === 'enter') {
-    hoverIndex.value = index
-  } else {
-    hoverIndex.value = -1
-  }
-  await nextTick()
-  selectNavTab(tabItemRefs.value[navTabs.activeIndex])
-}
 /**
  * @description 删除tab-item
  * @param index
@@ -109,8 +80,8 @@ const closeTab = (index: number) => {
 
 // 鼠标右键点击时
 const onMouseRight = (index: number, data: any) => {
-  mouseRightState.index = index;
-  mouseRightState.data = data;
+  contextMenuState.activeIndex = index;
+  contextMenuState.route = data;
 };
 /**
  * @description 鼠标右键菜单点击
@@ -128,15 +99,15 @@ const onMouseRightMenu = (status: number) => {
       isCurrentPageReload.value = false;
     });
   } else if (status === 2) {
-    closeTab(mouseRightState.index);
+    closeTab(contextMenuState.activeIndex);
   } else if (status === 3) {
     fullScreen();
     isLayoutFullScreen.value = true;
-    router.push(mouseRightState.data.path);
+    router.push(contextMenuState.route.path);
   } else if (status === 4) {
-    const tabs = navTabs.tabList.filter((_tab: RouteLocationNormalized, index) => index === mouseRightState.index);
+    const tabs = navTabs.tabList.filter((_tab: RouteLocationNormalized, index) => index === contextMenuState.activeIndex);
     navTabs.assign(tabs)
-    router.push(mouseRightState.data.path);
+    router.push(contextMenuState.route.path);
   } else if (status === 5) {
     navTabs.assign([])
     router.push("/home");
@@ -147,27 +118,13 @@ const onMouseRightMenu = (status: number) => {
  * 鼠标滚轮滚动
  */
 const handleTabsWheel = (event: any) => {
-  onWheelDelta(event, (delta: any) => {
+  onWheelDelta(event, () => {
     if (tabsRefs.value === null) return;
-    // 最大滚动距离
-    const maxScrollLeft =
-      tabsRefs.value.scrollWidth - tabsRefs.value.offsetWidth;
-    // 当最大滚动距离小于等于0时，不再滚动 减少性能消耗
-    if (maxScrollLeft <= 0) return;
-    // console.log("handleTabsWheel", maxScrollLeft, event);
-    if (delta > 0) {
-      // 滚轮向上滚动
-      tabsRefs.value.scrollLeft -= 50;
-      if (tabsRefs.value.scrollLeft <= 0) {
-        tabsRefs.value.scrollLeft = 0;
-      }
-    } else {
-      // 滚轮向下滚动
-      tabsRefs.value.scrollLeft += 50;
-      if (tabsRefs.value.scrollLeft >= maxScrollLeft) {
-        tabsRefs.value.scrollLeft = maxScrollLeft;
-      }
-    }
+    const {deltaY} = event;
+    tabsRefs.value.scrollBy({
+      left: deltaY, // 水平滚动
+      behavior: 'smooth', // 平滑滚动
+    });
   });
 };
 
@@ -195,7 +152,7 @@ const onWheelDelta = (e: any, cb: any) => {
  */
 const overLayDisabled = (type: OverlayType): boolean => {
   if (type === 'refresh') {
-    return navTabs.activeIndex !== mouseRightState.index
+    return navTabs.activeIndex !== contextMenuState.activeIndex
   }
   if (['close', 'closeOther', 'closeAll'].includes(type)) {
     return navTabs.tabList.length === 1
@@ -212,9 +169,6 @@ const handleTabTo = (route: RouteLocationNormalized) => {
 const selectNavTab = (dom: HTMLDivElement) => {
   if (!dom) return
   // console.log('dom -->', dom.offsetLeft, dom.clientWidth)
-
-  tagState.x = dom.offsetLeft
-  tagState.width = dom.clientWidth
 
   let scrollLeft = dom.offsetLeft + dom.clientWidth - tabsRefs.value.clientWidth
   if (dom.offsetLeft < tabsRefs.value.scrollLeft) {
@@ -250,13 +204,6 @@ defineOptions({
         @wheel="handleTabsWheel"
       >
         <div
-          class="nav-tabs-active-box"
-          :style="{
-            width: `${tagState.width}px`,
-            transform: `translateX(${tagState.x}px)`,
-          }"
-        ></div>
-        <div
           v-for="(item, index) in navTabs.tabList"
           :key="item.meta.title"
           :ref="el => setItemRefs(el as HTMLDivElement,index)"
@@ -267,25 +214,17 @@ defineOptions({
           }"
           @contextmenu.prevent="onMouseRight(index, item)"
           @click="handleTabTo(item)"
-          @mouseenter="changeOffsetLeft(index,'enter')"
-          @mouseleave="changeOffsetLeft(index,'level')"
         >
           <component
             :is="antIcons['HomeOutlined']"
             class="text-xs mr-1.5"
           />
           {{ item.meta.title }}
-          <transition
-            name="fade"
-            @after-leave="selectNavTab(tabItemRefs[navTabs.activeIndex])"
-            @after-enter="selectNavTab(tabItemRefs[navTabs.activeIndex])"
-          >
-            <close-outlined
-              v-if="navTabs.tabList.length !== 1 && (navTabs.activeIndex === index || hoverIndex === index)"
-              class="nav-tag-close"
-              @click.stop="closeTab(index)"
-            />
-          </transition>
+          <close-outlined
+            v-if="navTabs.tabList.length !== 1"
+            class="nav-tag-close"
+            @click.stop="closeTab(index)"
+          />
         </div>
       </div>
       <template #overlay>
